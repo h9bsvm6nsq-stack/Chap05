27d1c4a5e8cb6ab7f59b312ce07c6bcb7457d6e1
h9bsvm6nsq-stack <h9bsvm6nsq@privaterelay.appleid.com>
2026-02-01 15:31:54 -0600

    Start

---
+++ ModifyThis10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double sales = 0.0;
+    double rate = 0.0;
+    char code = ' ';
+
+    cout << "Sales: ";
+    cin >> sales;
+    cout << "Code (1, 2, 3, or 4): ";
+    cin >> code;
+
+    if (code == '1' || code == '3')
+        rate = 0.2;
+    else
+        rate = 0.15;
+    //end if
+
+    //calculate and display the projected sales amount
+    sales = sales + sales * rate;
+    cout << fixed << setprecision(2);
+    cout << "Projected sales: " << sales << endl;
+
+    return 0;
+}
